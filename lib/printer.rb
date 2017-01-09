1c5aaf47b4448253d8caebc9a0e015ec95292bd0
lister <simon@lisuna.org>
2017-01-09 20:46:36 +0100

    Fix codeship tests.

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,5 +1,10 @@
 require_relative 'printer/base'
-require_relative 'printer/sdl'
+
+require_relative 'printer/stairs'
+
+if ENV["APP_ENV"] == "development"
+  require_relative 'printer/sdl'
+end
 
 module Printer
 
